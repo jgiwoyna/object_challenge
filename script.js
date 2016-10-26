d54acb10bc148c6fd063e1ac0647a554247e0fa5
Joseph Giwoyna <josephgiwoyna@Josephs-MacBook-Pro.local>
2016-10-26 17:20:52 -0500

    Inital Commit

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+function State(state, revenue, locationCount) {
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+  this.averageRevenue = function() {
+    return this.revenue/this.locationCount;
+  }
+}
 // object instances
+var Illinois = new State('IL', 5000, 12);
+var Minnesota = new State('MN', 300, 3);
+var Nevada = new State ('NV', 25000, 1);
 
 // push object instances to locations array
+locations.push(Illinois);
+locations.push(Minnesota);
+locations.push(Nevada);
+
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
